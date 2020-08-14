dec1d644ee61d7c6e142bbefb916363a771adac9
Nirav Panchal <NiravP@sfwltd.co.uk>
2020-08-14 10:58:54 +0530

    Added extent reporting framework

--- com.selenium.cucumber/src/test/java/com/test/step/defintions/RunCukesTest.java
+++ com.selenium.cucumber/src/test/java/com/test/step/defintions/RunCukesTest.java
@@ -1,14 +1,35 @@
 package com.test.step.defintions;
 
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
+import com.cucumber.listener.Reporter;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "html:target/cucumberHtmlReport" },
+@CucumberOptions(plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html" },
 		// pretty:target/cucumber-json-report.json
 		features = "classpath:features")
 
 public class RunCukesTest {
+
+	/*
+	 * public static void writeExtentReport() {
+	 * Reporter.loadXMLConfig("src/extent-config.xml"); }
+	 */
+	@AfterClass
+	public static void writeExtentReport() {
+		Reporter.loadXMLConfig("src/extent-config.xml");
+		Reporter.setSystemInfo("User Name", System.getProperty("user.name"));
+		Reporter.setSystemInfo("Time Zone", System.getProperty("user.timezone"));
+		Reporter.setSystemInfo("Application Name", "Salesforce CRM");
+		Reporter.setSystemInfo("Operating System Type", System.getProperty("os.name").toString());
+		Reporter.setSystemInfo("Environment", "QA");
+		Reporter.setTestRunnerOutput("Test Execution Cucumber Report");
+		Reporter.setSystemInfo("Selenium", "3.11.0");
+		Reporter.setSystemInfo("Maven", "3.7.0");
+		Reporter.setSystemInfo("Java Version", "1.8.0_231");
+	}
 }
